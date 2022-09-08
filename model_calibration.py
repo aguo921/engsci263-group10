94f9d4f08b5afa7382e75326cf3383b3a6ee9473
Angela Guo <aguo921@aucklanduni.ac.nz>
2022-09-08 17:04:31 +1200

    Comment model_calibration.py script

--- model_calibration.py
+++ model_calibration.py
@@ -13,7 +13,7 @@ def load_pressure_data():
         t : array-like
             Vector of times (years) at which measurements were taken.
         p : array-like
-            Vector of Fluid pressure in reservoir (bar).
+            Vector of fluid pressure in reservoir (bars).
     '''
 
     t, p = np.genfromtxt("sb_pres.txt", delimiter=",", skip_header=1).T
@@ -21,7 +21,7 @@ def load_pressure_data():
 
 
 def load_subsidence_data():
-    ''' Returns time and subsidenc from sb_pres.txt
+    ''' Returns time and subsidence from sb_pres.txt
 
         Parameters:
         -----------
@@ -32,7 +32,7 @@ def load_subsidence_data():
         t : array-like
             Vector of times (years) at which measurements were taken.
         s : array-like
-            Vector of subsidence in reservoir (bar).
+            Vector of subsidence in reservoir (m).
     '''
 
     t, s = np.genfromtxt("sb_disp.txt", delimiter=",", skip_header=1).T
@@ -78,47 +78,6 @@ def dq_dt(t, q):
     dqdt = np.append(dqdt, dqdt[-1])
     return dqdt
 
-def plot_reservoir_pressure_model():
-    ''' Plot the Reservoir Pressure ODE over top of the data.
-
-        Parameters:
-        -----------
-        none
-
-        Returns:
-        --------
-        none
-
-        Notes:
-        ------
-        This function called within if __name__ == "__main__":
-    '''
-    time, pres = load_pressure_data()
-    t0 = time[0]
-    t1 = time[-1]
-    dt = 0.1
-    t = time_range(t0, t1, dt)
-    q = interpolate_mass_extraction_rate(t)
-    dqdt = dq_dt(t, q)
-
-    a = 0.0015377621974240604  # lumped parameter for forced extraction term
-    b1 = 0.06624924440742241  # lumped parameter for recharge term (into reservoir)
-    c = 0.007883715689885993  # lumped parameter for slow drainage term
-    P0 = 56.26  # initial/ambient reservoir pressure
-
-    # find numerical solution for reservoir pressure
-    t, p = solve_reservoir_ode(reservoir_ode, t0, t1, dt, P0, q, dqdt, [a, b1, c, P0])
-
-    fig, ax = plt.subplots()
-
-    # plot the observations and the numerical data
-    ax.plot(time, pres, '.', t, p)
-    ax.set_ylabel('reservoir pressure [bars]')
-    ax.set_xlabel('time [years]')
-    ax.legend(("observations", "model"))
-    plt.show()
-
-
 def plot_model():
     ''' Plot the reservoir pressure and subsidence over top of the data.
 
@@ -143,25 +102,29 @@ def plot_model():
     L = 100  # thickness of mudstone
     Pm0 = 56.26  # initial/ambient mudstone pressure
 
-    a = 0.0016753161225277887
-    b1 = 0.07412774351741645
-    c = 0.007630305388155743
-    b2 = 0.024184021965282205
-    mv = 0.009539371878306748
-
+    # obtain time and pressure from the data
     time_pres, pres = load_pressure_data()
+
+    # obtain time and subsidence from the data
     time_sub, sub = load_subsidence_data()
+
+    # set the time range
     t0 = time_pres[0]
     t1 = time_sub[-1]
     dt = 1
-
     t = time_range(t0, t1, dt)
+
+    # obtain the mass extraction rate
     q = interpolate_mass_extraction_rate(t)
+
+    # obtain the rate of change of mass extraction rate
     dqdt = dq_dt(t, q)
 
+    # find numerical solution for reservoir and mudstone pressure
     _, P = solve_reservoir_ode(reservoir_ode, t0, t1, dt, P0, q, dqdt, [a, b1, c, P0])
     _, Pm = solve_mudstone_ode(mudstone_ode, t0, t1, dt, P, Pm0, [b2])
 
+    # find numerical solution for subsidence
     U = subsidence_eqn(Pm, Pm0, mv, L)
 
     fig, (ax1, ax2) = plt.subplots(ncols=2)
@@ -178,32 +141,49 @@ def plot_model():
     ax2.set_xlabel('time [years]')
     ax2.legend(("observations", "model"))
 
+    # display the figure
     fig.set_size_inches(10, 6)
     plt.show()
 
 
 def plot_misfit(slow_drainage=False):
+    """ Plot the pressure misfit between the model and observations.
+
+        Parameters
+        ----------
+        slow_drainage : boolean (default = False)
+            True if slow drainage is considered in the model, false otherwise.
+
+    """
     if slow_drainage:
         a = 0.0015377621974240604  # lumped parameter for forced extraction term
         b1 = 0.06624924440742241  # lumped parameter for recharge term (into reservoir)
         c = 0.007883715689885993  # lumped parameter for slow drainage term
     else:
-        a = 0.0029363380044929903
-        b1 = 0.13757916629903563
-        c = 0
+        a = 0.0029363380044929903  # lumped parameter for forced extraction term
+        b1 = 0.13757916629903563  # lumped parameter for recharge term (into reservoir)
+        c = 0  # lumped parameter for slow drainage term
     P0 = 56.26  # initial/ambient reservoir pressure
 
+    # obtain time and pressure from the data
     time, pres = load_pressure_data()
+
+    # set the time range
     t0 = time[0]
     t1 = time[-1]
     dt = time[1] - time[0]
-
     t = time_range(t0, t1, dt)
+
+    # obtain the mass extraction rate
     q = interpolate_mass_extraction_rate(t)
+
+    # obtain the rate of change of mass extraction rate
     dqdt = dq_dt(t, q)
 
+    # find numerical solution for reservoir pressure
     _, P = solve_reservoir_ode(reservoir_ode, t0, t1, dt, P0, q, dqdt, [a, b1, c, P0])
 
+    # calculate the pressure misfit
     misfit = P - pres
 
     fig, (ax1, ax2) = plt.subplots(ncols=2)
@@ -225,6 +205,6 @@ def plot_misfit(slow_drainage=False):
 
 if __name__ == "__main__":
     plot_model()
-    # plot_misfit(True)
-    # plot_misfit(False)
+    plot_misfit(True)
+    plot_misfit(False)
 
