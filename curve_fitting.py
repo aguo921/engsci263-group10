e1998da6a0172e3552d1a189307f406ef4fe1889
Angela Guo <aguo921@aucklanduni.ac.nz>
2022-09-08 16:30:38 +1200

    Comment curve_fitting.py script

--- curve_fitting.py
+++ curve_fitting.py
@@ -16,16 +16,23 @@ def Pmodel(t, *pars):
         P : array-like
             Vector of reservoir pressure (bar).
     """
+    # obtain the mass extraction rate
     q = interpolate_mass_extraction_rate(t)
+
+    # obtain the rate of change of mass extraction rate
     dqdt = dq_dt(t, q)
 
+    # set the time range
     t0 = t[0]
     t1 = t[-1]
     dt = t[1]-t[0]
 
-    (a, b, c) = pars
-    P0 = 56.26
+    (a, b, c) = pars  # obtain the lumped parameter
+    P0 = 56.26  # initial/ambient reservoir pressure
+
+    # find numerical solution for reservoir pressure
     _, P = solve_reservoir_ode(reservoir_ode, t0, t1, dt, P0, q, dqdt, [a,b,c,P0])
+
     return P
 
 def Umodel(t, *pars):
@@ -43,9 +50,13 @@ def Umodel(t, *pars):
         U : Vector of subsidence (m).
 
     """
+    # obtain the mass extraction rate
     q = interpolate_mass_extraction_rate(t)
+
+    # obtain the rate of change of mass extraction rate
     dqdt = dq_dt(t, q)
 
+    # set the time range
     t0 = t[0]
     t1 = t[-1]
     dt = t[1] - t[0]
@@ -57,10 +68,6 @@ def Umodel(t, *pars):
     P0 = 56.26
     Pm0 = 56.26
     L = 100
-
-    a = 0.0016753161225277887
-    b1 = 0.07412774351741645
-    c = 0.007630305388155743
 
     # parameters to be fitted
     (b2, mv) = pars
@@ -84,16 +91,21 @@ def calibrate_reservoir_pressure():
         c : float
             Fitted lumped parameter for slow drainage term.
     """
+    # obtain time and subsidence from the data
     to,Po = load_pressure_data()
 
-    # interpolate mass extraction rate data and find derivative
+    # obtain the mass extraction rate
     q = interpolate_mass_extraction_rate(to)
+
+    # obtain the rate of change of mass extraction
     dqdt = dq_dt(to, q)
 
     p0 = [1.62e-3,7.15e-2,0.007]  # original guess
-    constants = curve_fit(Pmodel, to[:-10], Po[:-10], p0)
-    (a, b, c) = constants[0]
-    P0 = 56.26
+    constants = curve_fit(Pmodel, to, Po, p0)  # curve fit the data
+    (a, b, c) = constants[0]  # extract fitted parameters
+    P0 = 56.26  # initial/ambient reservoir pressure
+
+    # find numerical solution for reservoir pressure
     ti,Pi = solve_reservoir_ode(reservoir_ode, to[0], to[-1], to[1]-to[0], P0, q, dqdt, [a,b,c,P0])
 
     fig, ax = plt.subplots()
@@ -122,6 +134,7 @@ def calibrate_subsidence():
         mv : float
             Fitted compressibility of mudstone.
     """
+    # obtain time and subsidence from the data
     to, Uo = load_subsidence_data()
 
     # interpolate subsidence at defined time intervals
@@ -129,12 +142,16 @@ def calibrate_subsidence():
     U = np.interp(t, to, Uo)
 
     p0 = [0.0355,0.0076]  # initial guess
-    constants = curve_fit(Umodel, t[:-10], U[:-10], p0)
+    constants = curve_fit(Umodel, t, U, p0)  # curve fit the data
     (b2, mv) = constants[0]  # extract fitted parameters
 
+    # obtain the mass extraction rate
     q = interpolate_mass_extraction_rate(t)
+
+    # obtain the rate of change of mass extraction rate
     dqdt = dq_dt(t, q)
 
+    # set the time range
     t0 = t[0]
     t1 = t[-1]
     dt = t[1] - t[0]
@@ -147,11 +164,7 @@ def calibrate_subsidence():
     Pm0 = 56.26
     L = 100
 
-    a = 0.0016753161225277887
-    b1 = 0.07412774351741645
-    c = 0.007630305388155743
-
-    # solve for subsidence
+    # numerically solve for subsidence
     t, P = solve_reservoir_ode(reservoir_ode, t0, t1, dt, P0, q, dqdt, [a, b1, c, P0])
     t, Pm = solve_mudstone_ode(mudstone_ode, t0, t1, dt, P, Pm0, [b2])
     subsidence = subsidence_eqn(Pm, Pm0, mv, L)
@@ -161,7 +174,7 @@ def calibrate_subsidence():
     # plot the observations and the numerical data
     ax.plot(to, Uo, '.', t, subsidence)
     ax.set_ylabel('subsidence [m]')
-    ax.set_xlabel('time, [yr]')
+    ax.set_xlabel('time [years]')
     ax.legend(("observations", "model"))
     plt.show()
 
@@ -173,7 +186,5 @@ def calibrate_subsidence():
 
 
 if __name__ == "__main__":
-    # pars = calibrate_reservoir_pressure()
-    # print(pars)
-    pars = calibrate_subsidence()
-    print(pars)
+    print(calibrate_reservoir_pressure())
+    print(calibrate_subsidence())
